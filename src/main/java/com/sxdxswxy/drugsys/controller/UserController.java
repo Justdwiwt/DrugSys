071345862dced679f7c9bfbe7cdfa8fb86e38878
Justdwiwt <len_master@hotmail.com>
2018-11-09 20:52:08 +0800

    add title & foot pages

--- src/main/java/com/sxdxswxy/drugsys/controller/UserController.java
+++ src/main/java/com/sxdxswxy/drugsys/controller/UserController.java
@@ -1,33 +1,33 @@
-//package com.sxdxswxy.drugsys.controller;
-//
-//import com.sxdxswxy.drugsys.pojo.User;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Controller
-//@RequestMapping("/user")
-//public class UserController {
-//
-//    @RequestMapping("/{id}")
-//    public String getUser(@PathVariable int id, Model model) {
-//        model.addAttribute("user", new User(id, "name0", 20, '1',
-//                2015330200, "软件工程", "行知8"));
-//        return "/user/detail";
-//    }
-//
-//    @RequestMapping("/list")
-//    public String listUser(Model model) {
-//        List<User> userList = new ArrayList<>();
-//        for (int i = 1; i < 10; i++)
-//            userList.add(new User(i, "name" + i, 20 + i, '1',
-//                    201533020 + i, "软件工程", "行知8"));
-//        model.addAttribute("users", userList);
-//        return "/user/list";
-//    }
-//
-//}
+package com.sxdxswxy.drugsys.controller;
+
+import com.sxdxswxy.drugsys.pojo.UserInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/{userId}")
+    public String getUser(@PathVariable int userId, Model model) {
+        model.addAttribute("user", new UserInfo(userId, 20, '1', "1@1.com",
+                "123450", "软件工程", "行知8"));
+        return "/user/detail";
+    }
+
+    @RequestMapping("/list")
+    public String listUser(Model model) {
+        List<UserInfo> userInfoList = new ArrayList<>();
+        for (int i = 1; i < 10; i++)
+            userInfoList.add(new UserInfo(i, 20 + i, '1', "1" + i + "@1.com",
+                    "123450" + i, "软件工程", "行知8"));
+        model.addAttribute("userInfos", userInfoList);
+        return "/user/list";
+    }
+
+}
--- src/main/java/com/sxdxswxy/drugsys/pojo/UserInfo.java
+++ src/main/java/com/sxdxswxy/drugsys/pojo/UserInfo.java
@@ -10,6 +10,16 @@ public class UserInfo {
     private String userPro;
     private String userAddress;
 
+    public UserInfo(int userId, int userAge, char userSex, String userEmail, String userPhone, String userPro, String userAddress) {
+        this.userId = userId;
+        this.userAge = userAge;
+        this.userSex = userSex;
+        this.userEmail = userEmail;
+        this.userPhone = userPhone;
+        this.userPro = userPro;
+        this.userAddress = userAddress;
+    }
+
     public int getUserId() {
         return userId;
     }
